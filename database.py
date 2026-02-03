39307919c65cba749a1029f5f5e2b8df4ada32d8
diego-4545 <120226370+diego-4545@users.noreply.github.com>
2026-02-02 22:09:34 -0600

    Change database URL to use PostgreSQL environment variable

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "os.getenv("postgresql://pruebaa_onlk_user:U0nqQV6DrKlGw707RmudYR4v6nimLhAv@dpg-d60n87l6ubrc739b1hh0-a/pruebaa_onlk")"
 
 engine = create_engine(
     DATABASE_URL,
