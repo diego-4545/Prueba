55704ad1622a100667647f64d265cff3caa83cea
diego-4545 <120226370+diego-4545@users.noreply.github.com>
2026-02-02 22:35:16 -0600

    Print DATABASE_URL for debugging purposes
    Added print statement to output DATABASE_URL.

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("postgresql://pruebaa_onlk_user:U0nqQV6DrKlGw707RmudYR4v6nimLhAv@dpg-d60n87l6ubrc739b1hh0-a/pruebaa_onlk")
+print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
