00319925965cffea4b70e84143f38609d26ffc68
diego-4545 <120226370+diego-4545@users.noreply.github.com>
2026-02-02 22:49:52 -0600

    Update DATABASE_URL retrieval and session creation
    Refactor database URL handling and improve readability.

--- database.py
+++ database.py
@@ -1,11 +1,20 @@
-import os
-print("DATABASE_URL =", os.getenv("DATABASE_URL"))
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = os.getenv("postgresql://pruebaa_onlk_user:U0nqQV6DrKlGw707RmudYR4v6nimLhAv@dpg-d60n87l6ubrc739b1hh0-a/pruebaa_onlk")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL no está definida")
+
 print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
